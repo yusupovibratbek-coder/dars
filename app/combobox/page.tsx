50f16ba41d540d2d60734342f39095ad6ea4bbc9
yusupovibratbek-coder <yusupovibratbek18@gmail.com>
2026-04-02 04:12:20 -0700

    feat: initial commit

---
+++ app/combobox/page.tsx
@@ -0,0 +1,9 @@
+import { ComboboxBasic } from "../components/ComboboxBasic";
+
+export default async function Posts() {
+    return(
+        <div>
+            <div><ComboboxBasic /></div>
+        </div>
+    )
+}
---
+++ app/json/page.tsx
@@ -0,0 +1,26 @@
+
+import { discoverValidationDepths } from "next/dist/server/app-render/instant-validation/instant-validation";
+import Link from "next/link";
+import { json } from "stream/consumers";
+
+export default async function Posts() {
+    const url = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await url.json();
+
+    return (
+        <div className="bg-white min-h-screen">
+            <div className="">
+                <h1 className="text-4xl text-black  font-mono p-24 text-center">Postlar ro'yxati</h1>
+                {posts.map((items: any) => (
+                    <div key={items.id}>
+                        <h1 className="font-bold text-2xl text-gray-900 font-serif underline">{items.title}</h1>
+                        <h1 className="text-black">{items.body}</h1>
+                    </div>
+                ))}
+            </div>
+            <div>
+                <Link href={'/combobox'}>Combobox</Link>
+            </div>
+        </div>
+    )
+}
