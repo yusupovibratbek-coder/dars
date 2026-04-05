5e7d8ba0e5a0c6cfe9c41b536a801f5652d834f0
yusupovibratbek-coder <yusupovibratbek18@gmail.com>
2026-04-05 11:29:53 -0700

    yangilandi

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="bg-white min-h-screen">
-      <div className="flex justify-around p-10 bg-green-600">
+      <div className="flex justify-around p-10 bg-green-600 border-white border-3 rounded-4xl ">
         <Link className="text-4xl text-black font-bold font-sans drop-shadow-lg shadow-gray-500 border-black rounded-lg bg-white border-1 w-30 text-center drop-shadow-lg shadow-gray-500" href={'/json'}>JSON</Link>
         <Link className="text-4xl text-black font-bold font-sans drop-shadow-lg shadow-gray-500 border-black bg-white rounded-lg border-1 w-30 text-center drop-shadow-lg shadow-gray-500" href={'/json(2)'}>JSON</Link>
         <Link className="text-4xl font-bold border-black border-1 rounded-lg w-50 text-center font-sans bg-white drop-shadow-lg shadow-gray-500" href={'/combobox'}>Combobox</Link>
@@ -15,7 +15,7 @@ export default function Home() {
 
       </div>
 
-      <div className="flex justify-around p-10 bg-black border-white border-2 rounded-4xl ">
+      <div className="flex justify-around p-10 bg-black border-white border-3 rounded-4xl ">
         <Button className="text-5xl font-serif font-bold bg-blue-800 flex justify-around p-10 w-65  ">
           <Link href={'/popover'}>Button</Link>
         </Button>
